04ff361daa5301492d5fd0fe02bb5f1752675d1d
Johan Eriksson <jeriksson@stratiteq.com>
2020-07-14 23:29:46 +0200

    textchanges

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Om om om</h1>
+    <h2>johan</h2>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Välkommen till Johan Testar nya Static Web App" />
   </div>
 </template>
 
